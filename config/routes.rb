b40f8b82351a0c44b09e097293c0f255d36c0165
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-02 18:52:07 +0000

    use resources instead of writing out all routes by hand

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,39 @@
 Rails.application.routes.draw do
   # Routes for the Movie resource:
-  
+  resources :movies
+
   # CREATE
-  post("/movies" => "movies#create", as: :movies)
-  get("/movies/new" => "movies#new", as: :new_movie)
+  # post("/movies" => "movies#create", as: :movies)
+  # get("/movies/new" => "movies#new", as: :new_movie)
   
-  # READ
-  get("/movies" => "movies#index" )
-  get("/movies/:id" => "movies#show", as: :movie)
+  # # READ
+  # get("/movies" => "movies#index" )
+  # get("/movies/:id" => "movies#show", as: :movie)
   
-  # UPDATE
-  patch("/movies/:id" => "movies#update")
-  get("/movies/:id/edit" => "movies#edit", as: :edit_movie)
+  # # UPDATE
+  # patch("/movies/:id" => "movies#update")
+  # get("/movies/:id/edit" => "movies#edit", as: :edit_movie)
   
-  # DELETE
-  delete("/movies/:id" => "movies#destroy")
+  # # DELETE
+  # delete("/movies/:id" => "movies#destroy")
 
-  #CREATE
-  post("/directors" => "directors#create", as: :directors)
-  get("/directors/new" => "directors#new", as: :new_director)
+  # Routes for the Movie resource:
+  resources :directors
 
-  #READ
-  get("/directors" => "directors#index")
-  get("/directors/:id" => "directors#show", as: :director)
+  # #CREATE
+  # post("/directors" => "directors#create", as: :directors)
+  # get("/directors/new" => "directors#new", as: :new_director)
 
-  #UPDATE
-  patch("/directors/:id" => "directors#update")
-  get("/directors/:id/edit" => "directors#edit", as: :edit_director)
+  # #READ
+  # get("/directors" => "directors#index")
+  # get("/directors/:id" => "directors#show", as: :director)
 
-  #DELETE
-  delete("/directors/:id" => "directors#destroy")
+  # #UPDATE
+  # patch("/directors/:id" => "directors#update")
+  # get("/directors/:id/edit" => "directors#edit", as: :edit_director)
+
+  # #DELETE
+  # delete("/directors/:id" => "directors#destroy")
 
   root "movies#index"
 end
